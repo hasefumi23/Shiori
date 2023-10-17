98a26c64376b37db2f5d778f74822b8b57e5b21c
hasefumi23 <hasefumi23@gmail.com>
2023-10-17 21:26:22 +0900

    yarn buildが成功する状態にする

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,29 @@
 import { crx } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
-import { join } from 'path';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 import manifest from './src/manifest';
-import removeSrcFromHtmlPaths from './utils/plugins/removeSrcFromHtmlPaths';
 
 export default defineConfig({
+  // @see https://github.com/crxjs/chrome-extension-tools/issues/696
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
+  // prevent src/ prefix on extension urls
+  root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'public'),
   build: {
+    outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
-      input: {
-        welcome: join(__dirname, 'src/welcome/welcome.html'),
-      },
       output: {
         chunkFileNames: 'assets/chunk-[hash].js',
       },
     },
   },
-  plugins: [react(), crx({ manifest }), removeSrcFromHtmlPaths()],
+  plugins: [react(), crx({ manifest })],
 });
